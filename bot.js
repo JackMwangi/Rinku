2dab35b01afc830ff5d7bfca83da7f53df526ab3
Amos Omondi <amos.omondi@andela.com>
2017-01-17 12:54:00 +0300

    (feat) Save links to db

--- bot.js
+++ bot.js
@@ -1,17 +1,11 @@
 require('dotenv').config();
 const Botkit = require('botkit');
-const mongoose = require('mongoose');
 
-const link = require('./models/link');
 const logger = require('./lib/logger')();
 const utils = require('./lib/utils')
 
-const dbUrl = process.env.MONGODB_REMOTE_URL;
-
-mongoose.connect(dbUrl);
-
 const controller = Botkit.slackbot({
-  debug: true,
+  debug: false,
 });
 
 controller.spawn({
@@ -21,9 +15,7 @@ controller.spawn({
 const { hears } = controller;
 
 controller.on('ambient',function(bot,message) {
-  var textMessage = message.text.replace("<","").replace(">","");
-  var result = utils.matchUrl(textMessage)
-  logger.info(`matched==> : ${result}`);
+  utils.matchUrl(message)
 });
 
 hears(['hello'], 'direct_message,direct_mention,mention', (bot, message) => {
--- models/link.js
+++ models/link.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const linkSchema = new Schema({
-  url: String, // the extracted URL
+  urls: Array, // the extracted URLs
   text: String, // the whole message
   user: String, // the user who posted the message
   channel: String, // the channel it wacleas posted to
