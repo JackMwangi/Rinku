e1894bbd84c397d3461adc6a9784ddfef418dcc3
Amos Omondi <amos.omondi@andela.com>
2017-01-17 12:00:16 +0300

    Merge develop into ft-setup-mongo

--- bot.js
+++ bot.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
-
 const Botkit = require('botkit');
 const mongoose = require('mongoose');
 
 const link = require('./models/link');
+const logger = require('./lib/logger')();
+const utils = require('./lib/utils')
+
 const dbUrl = process.env.MONGODB_REMOTE_URL;
 
-// DB Connection
 mongoose.connect(dbUrl);
 
 const controller = Botkit.slackbot({
@@ -18,6 +19,12 @@ controller.spawn({
 }).startRTM();
 
 const { hears } = controller;
+
+controller.on('ambient',function(bot,message) {
+  var textMessage = message.text.replace("<","").replace(">","");
+  var result = utils.matchUrl(textMessage)
+  logger.info(`matched==> : ${result}`);
+});
 
 hears(['hello'], 'direct_message,direct_mention,mention', (bot, message) => {
   bot.reply(message, 'YO!');
