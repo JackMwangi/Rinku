1e5bba4588bec413219662fd46aa58ad9753aa05
Amos Omondi <amos.omondi@andela.com>
2017-01-19 12:14:53 +0300

    (chore) configure eslint and fix lint issues

--- models/link.js
+++ models/link.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
 const linkSchema = new Schema({
@@ -6,7 +7,7 @@ const linkSchema = new Schema({
   text: String, // the whole message
   user: String, // the user who posted the message
   channel: String, // the channel it wacleas posted to
-  timestamp: Date // the timestamp of the posting
+  timestamp: Date, // the timestamp of the posting
 });
 
 const Link = mongoose.model('Link', linkSchema);
